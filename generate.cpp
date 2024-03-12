6b3597c6462c8aac736f3e3750321956afd08373
arnavmishra1 <arnav9@outlook.com>
2024-03-12 16:16:23 -0700

    frame

---
+++ generate.cpp
@@ -0,0 +1,22 @@
+// Author: Arnav Mishra
+// CECS 325-02 Prog 3
+// Due 03/12/2024
+
+// I certify that this program is my own original work.
+// I did not copy any part of this program from any other source.
+// I further certify that I typed each and every line of code in this program.
+
+#include <iostream>
+
+using namespace std;
+
+int main(int args, char *arglist[]) {
+    if (args != 4) {
+        cout << "Usage: generate [count] [min] [max]" << endl;
+    } else {
+        //program
+        
+    }
+
+    return 0;
+}
