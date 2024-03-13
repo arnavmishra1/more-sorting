1fe4a56eceedbf9fe00a3406e1ea2f544fcb4ef4
arnavmishra1 <arnav9@outlook.com>
2024-03-12 17:46:19 -0700

    generate working

--- generate.cpp
+++ generate.cpp
@@ -7,18 +7,34 @@
 // I further certify that I typed each and every line of code in this program.
 
 #include <iostream>
+#include <fstream>
+#include <time.h>
 
 using namespace std;
 
-int main(int args, char *arglist[]) {
+int main(int args, char *argv[]) {
     if (args != 4) {
         cout << "Usage: generate [length] [min] [max]" << endl;
     } else {
-        int length = stoi(arglist[1]);
-        int min = stoi(arglist[2]);
-        int max = stoi(arglist[3]);
-        int x = rand() % (max - min + 1) + min;
-        cout << x << endl;
+        int length = stoi(argv[1]);
+        int min = stoi(argv[2]);
+        int max = stoi(argv[3]);
+
+        int numbers[length];
+
+        srand(time(NULL));
+
+        for (int i = 0; i < length; i++) {
+            numbers[i] = rand() % (max - min + 1) + min;
+        }
+
+        //write to file
+        ofstream file("numbers.dat");
+        for (int a : numbers) {
+            file << a << endl;
+        }
+        file.close();
+        
     }
 
     return 0;
