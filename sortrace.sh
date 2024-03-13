acc61bf4eebab514053b0096d10d308ebf743949
arnavmishra1 <arnav9@outlook.com>
2024-03-12 20:27:30 -0700

    shell working

--- sortrace.sh
+++ sortrace.sh
@@ -5,7 +5,7 @@
 # $ sortrace.sh >> sortrace.log & # this may take an hour
 echo Generating 1000000 random numbers
 sleep 1
-generate 10000 -100000 100000 # you have to write generate.cpp
+./generate 10000 -100000 100000 # you have to write generate.cpp
 sleep 1
 echo Starting system sort
 sleep 1
@@ -13,6 +13,6 @@ sleep 1
 sleep 1
 echo Starting mysort
 sleep 1
-{ time mysort numbers.dat mysort.out; } 2>> sortrace.log # you have to write mysort.cpp
+{ time ./mysort numbers.dat mysort.out; } 2>> sortrace.log # you have to write mysort.cpp
 sleep 1
 sort -c -n mysort.out 2>> sortrace.log # verify file is sorted
